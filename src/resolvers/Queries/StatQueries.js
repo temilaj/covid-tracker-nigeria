b8818f09bca14b8c9eda302d4be5c5e9fbdef21b
temilaj <temi@temilajumoke.com>
2020-05-08 15:19:13 +0100

    add results by state query

--- src/resolvers/Queries/StatQueries.js
+++ src/resolvers/Queries/StatQueries.js
@@ -16,6 +16,39 @@ const StatQueries = {
     );
     return latest;
   },
+
+  async resultsByState(parent, args, ctx, info) {
+    // TODO cache results in redis
+    const timelines = await ctx.db.query.timelines(
+      { orderBy: 'dateRecorded_ASC' },
+      '{ confirmed recoveries deaths dateRecorded state { slug } }',
+    );
+    const latest = timelines.reduce((acc, timeline, index) => {
+      const { slug } = timeline.state;
+      if (acc[slug] && acc[slug].confirmed >= 0) {
+        acc[slug].confirmed += timeline.confirmed;
+        acc[slug].recoveries += timeline.recoveries;
+        acc[slug].deaths += timeline.deaths;
+      } else {
+        acc[slug] = {};
+        acc[slug].confirmed = timeline.confirmed;
+        acc[slug].recoveries = timeline.recoveries;
+        acc[slug].deaths = timeline.deaths;
+      }
+      if (index === timelines.length - 1) {
+        acc.lastUpdated = timeline.dateRecorded;
+      }
+      return acc;
+    }, {});
+    const result = [];
+    Object.entries(latest).map(([key, value]) => {
+      if (typeof value === 'object') {
+        return result.push({ state: key, ...value });
+      }
+    });
+    result.sort((a, b) => (a.confirmed > b.confirmed ? -1 : 1));
+    return result;
+  },
 };
 
 module.exports = StatQueries;
